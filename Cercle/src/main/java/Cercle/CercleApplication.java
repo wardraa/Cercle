9dcf61e0334ff85cf4daf868de1704c3efff697f
e0900319 <wardraa@hotmail.fr>
2015-09-26 09:39:34 +0200

    2eme commit avec tout le code

---
+++ Cercle/src/main/java/Cercle/CercleApplication.java
@@ -0,0 +1,12 @@
+package Cercle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CercleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CercleApplication.class, args);
+    }
+}
---
+++ Cercle/src/test/java/Cercle/CercleApplicationTests.java
@@ -0,0 +1,18 @@
+package Cercle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CercleApplication.class)
+@WebAppConfiguration
+public class CercleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
